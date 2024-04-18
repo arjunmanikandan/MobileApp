4a77c553b48852d4981d4db3f41ea8891ac98dda
arjunmanikandan <arjunmanikandan319@gmail.com>
2024-04-18 12:50:08 +0530

    Used gpt-4 model for summarization and entity extraction

--- summary.py
+++ summary.py
@@ -1,56 +1,29 @@
+from openai import AzureOpenAI
 import json
-# Use a pipeline as a high-level helper
-from transformers import pipeline
-from transformers import logging
-logging.set_verbosity_error()
 
 def summarize():
-    # Open the JSON file in read mode
-    with open(r"C:\Users\ADMIN\Videos\mobile_app\medical_summarization\text.json", "r") as file:
-        # Load the JSON data
-        data = json.load(file)
+    with open(r"C:\Users\ADMIN\Videos\mobile_app\medical_summarization\text.json","r") as file:
+        result = json.load(file)
 
-    # # Access the data from the JSON file
-    # print(data)
-    final_text = data["text"]
-    # print(result)
+    final_result = result["text"]
+        
+    client = AzureOpenAI(
+    azure_endpoint = "https://rt-interns.openai.azure.com/", 
+    api_key="",  
+    api_version="2024-02-01"
+    )
 
+    response = client.chat.completions.create(
+        model="gpt-4-vision-preview", # model = "deployment_name".
+        max_tokens=4096,
+        messages=[
+    {"role": "system", "content":final_result},
+    {"role": "user", "content": "Give a summary in 50 words and extract entities like name,gender,age,diseasetype,symptoms"},
+        ])
 
-    pipe1 = pipeline("token-classification", model="dslim/bert-large-NER", aggregation_strategy='simple',)
-
-    pipe2 = pipeline("token-classification", model="Clinical-AI-Apollo/Medical-NER", aggregation_strategy='simple')
-
-    text = final_text
-
-    result1 = pipe1(text)
-
-    result2 = pipe2(text)
-
-    result1.extend(result2)
-
-    new_list = []
-    for item in result1:
-        dict1 = {item["entity_group"]:item["word"]}
-        # print(dict1)
-        new_list.append(dict1)
+    data = {"text":response.choices[0].message.content}
 
     with open(r"C:\Users\ADMIN\Videos\mobile_app\medical_summarization\summary.json","w") as file:
-        json.dump(new_list,file)
+        json.dump(data,file)
 
-    print("Summarized")
-
-summarize()
-# column1 = []
-# for row in result:
-#     for i in row.keys():
-#         column1.append(i)
-
-# column2 = []
-# for row1 in result:
-#     for j in row1.values():
-#         column2.append(j)
-
-# for key,value in zip(column1,column2):
-#     print(key," ",value)
-
-
+    print("Summarized successfully")
